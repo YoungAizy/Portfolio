5e4ce270e2e75b15feb3420ad648f0acdc0c4619
Ayanda Marotya <ayandamarotya@gmail.com>
2022-09-06 04:32:44 +0200

    update intro

--- components/Intro.js
+++ components/Intro.js
@@ -5,9 +5,8 @@ export default function Intro(){
     return(
         <div className={styles.intro}>
         <div>
-        <h2>Hi, there!</h2>
-          <p> My name is Ayanda Marotya, I am a Software and Web developer with project experience both on frontend and backend technologies. I also provide SEO and Digital Marketing services.
-            My passion is to design optimised solutions with eye-catching designs. I am always open to grabbing new opportunities and learning new technologies. I am looking forward in hearing from you, and hopefully working together.
+        <h2>Ayanda Marotya</h2>
+          <p> Hi there!<br></br><br></br>Meet your coding buddy Ayanda Marotya. I am a Full Stack developer with hands on experience in web and android mobile development.  My first love is Java &#40;React.js being a close second&#41; and alongside it my passion to design optimised solutions with eye-catching designs. I love new challenges and learning new stuff and therefore I&#39;m always looking to expand my knowledge of the tech world and refine my skill.<br></br><br></br>Have a look at the <code>PROJECTS</code> section and send me an email if my work impresses you. I am looking forward to hearing from you, and hopefully working together :&#41;
         </p>
         </div>
         <Image src={"/display_pic.jpg"} alt="profile display picture" height={250} width={250} />
